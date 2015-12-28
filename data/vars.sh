23fc76bcc303ecd85e15c603ea64f9fe5a8c6d16
Sam Wan <sam.wan@emc.com>
2015-12-28 01:06:39 -0500

    Add sample data configuration

---
+++ data/vars.sh
@@ -0,0 +1,7 @@
+export UPSTREAM_GERRIT_HOST_PUBLIC_KEY="ssh-rsa xxxxxxxxxxxxxxxxxx"
+export GIT_EMAIL='sam.wan@emc.com'
+export GIT_NAME='Sam Wan'
+export UPSTREAM_GERRIT_USER=samwan
+export UPSTREAM_GERRIT_SSH_KEY_PATH=gerrit_key
+export JENKINS_SSH_KEY_PATH=jenkins_key
+export PUBLISH_HOST=127.0.0.1
